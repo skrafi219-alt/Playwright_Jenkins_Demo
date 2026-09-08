d63537b8387dc8dd0ea90a9acc7c5cc3f8a5eb49
Rafi Shaik <rafi.tgcs219@gmail.com>
2026-09-08 21:43:03 +0530

    added new end to end test

---
+++ tests/endtoend.spec.ts
@@ -0,0 +1,23 @@
+import {test,expect} from '@playwright/test';
+
+test('end to end UI', async ({page})=>{
+
+await page.goto("https://rahulshettyacademy.com/client/");
+await page.locator("#userEmail").fill("rafi.tgcs219@gmail.com");
+await page.locator("#userPassword").fill("MBxanptFWYri5$k");
+await page.locator("#login").click();
+const productName="ZARA COAT 3";
+await page.locator("div.card-body").first().waitFor();
+const products=page.locator("div.card-body");
+
+for(let i=0;i<=await products.count();i++){
+
+  if(await page.locator("div.card-body").locator("b").nth(i).textContent()===productName){
+
+    await page.locator("text=' Add To Cart'").nth(i).click();
+    break;
+  }
+}
+await page.pause();
+
+});
